d3124cd51b17d6c35c24632b7c06816988246af4
lucaskhane <dani.kn17.rjch@gmail.com>
2017-09-19 15:06:54 +0200

    Plugin para gestionar orientacion del movil y solucionado touch events en movil

--- virusBoardGame/platforms/browser/www/js/game.js
+++ virusBoardGame/platforms/browser/www/js/game.js
@@ -14,6 +14,8 @@ function playSound(soundResource){
 	}
 }
 
+//TELEFONO
+/**
 var cv, cx, objetos, objetoActual = null;
 var inicioX = 0, inicioY = 0;
 function actualizar(){
@@ -30,46 +32,116 @@ window.onload = function(){
 	cx = cv.getContext('2d');
 	objetos.push({
 		x: 0, y: 0,
-		width: 100, height: 200,
+		width: 200, height: 400,
 		color: '#00f'
 	});
 	objetos.push({
 		x: 300, y: 150,
-		width: 50, height: 100,
+		width: 100, height: 200,
 		color: '#f00'
 	});
 	objetos.push({
 		x: 120, y: 150,
-		width: 50, height: 100,
+		width: 100, height: 200,
 		color: '#0f0'
 	});
 
 	cv.ontouchstart = function(event) {
-	//cv.onmousedown = function(event) {
+		console.log("Onmousedown");
 		for (var i = 0; i < objetos.length; i++) {
-			if (objetos[i].x < event.clientX
-			  && (objetos[i].width + objetos[i].x > event.clientX)
-			  && objetos[i].y < event.clientY
-			  && (objetos[i].height + objetos[i].y > event.clientY)) {
+			if (objetos[i].x < event.layerX
+			  && (objetos[i].width + objetos[i].x > event.layerX)
+			  && objetos[i].y < event.layerY
+			  && (objetos[i].height + objetos[i].y > event.layerY)) {
 				objetoActual = objetos[i];
-				inicioY = event.clientY - objetos[i].y;
-				inicioX = event.clientX - objetos[i].x;
+				console.log("Objeto "+i+" TOCADO");
+				inicioY = event.layerY - objetos[i].y;
+				inicioX = event.layerX - objetos[i].x;
 				break;
 			}
 		}
 	}
 
 	cv.ontouchmove = function(event) {
-	//cv.onmousemove = function(event) {
+		console.log("Onmousemove");
 		if (objetoActual != null) {
-			objetoActual.x = event.clientX - inicioX;
-			objetoActual.y = event.clientY - inicioY;
+			objetoActual.x = event.layerX - inicioX;
+			objetoActual.y = event.layerY - inicioY;
 		}
 		actualizar();
 	}
 
 	cv.ontouchend = function(event) {
-	//cv.onmouseup = function(event) {
+		console.log("Onmouseup");
+		objetoActual = null;
+	}
+}
+**/
+
+//ORDENADOR
+
+var cv, cx, objetos, objetoActual = null;
+var inicioX = 0, inicioY = 0;
+function actualizar(){
+	cx.fillStyle = '#f0f0f0';
+	cx.fillRect(0,0,1000,700);
+	for (var i = 0; i < objetos.length; i++){
+		cx.fillStyle = objetos[i].color;
+		cx.fillRect(objetos[i].x, objetos[i].y, objetos[i].width, objetos[i].height);
+	}
+}
+window.onload = function(){
+	objetos = [];
+	cv = document.getElementById('canvas')
+	cx = cv.getContext('2d');
+	objetos.push({
+		x: 0, y: 0,
+		width: 200, height: 400,
+		color: '#00f'
+	});
+	objetos.push({
+		x: 300, y: 150,
+		width: 100, height: 200,
+		color: '#f00'
+	});
+	objetos.push({
+		x: 120, y: 150,
+		width: 100, height: 200,
+		color: '#0f0'
+	});
+
+	cv.onmousedown = function(event) {
+		console.log("Onmousedown");
+		console.log("Event: "+event);
+		//console.log("event.layerX: "+event.layerX);
+		//console.log("event.layerY: "+event.layerY);
+		//console.log("event.pageX: "+event.pageX);
+		//console.log("event.pageY: "+event.pageY);
+		for (var i = 0; i < objetos.length; i++) {
+			if (objetos[i].x < event.layerX
+			  && (objetos[i].width + objetos[i].x > event.layerX)
+			  && objetos[i].y < event.layerY
+			  && (objetos[i].height + objetos[i].y > event.layerY)) {
+				objetoActual = objetos[i];
+				console.log("Objeto "+i+" TOCADO");
+				inicioY = event.layerY - objetos[i].y;
+				inicioX = event.layerX - objetos[i].x;
+				break;
+			}
+		}
+	}
+
+	cv.onmousemove = function(event) {
+		console.log("Onmousemove");
+		if (objetoActual != null) {
+			objetoActual.x = event.layerX - inicioX;
+			objetoActual.y = event.layerY - inicioY;
+		}
+		actualizar();
+	}
+
+	cv.onmouseup = function(event) {
+		console.log("Onmouseup");
 		objetoActual = null;
 	}
 }
@@ -88,6 +160,9 @@ function takeCard(){
 
 $(document).ready(function(){
 	console.log("Document Ready");
+	console.log("Orientation before lock is: "+screen.orientation.type);
+	//Dara error en las pruebas de ordenador, pero no deberia ocurrir nada mas
+	screen.orientation.lock('landscape');
 
 	simularDatosIniciales();
 
--- virusBoardGame/www/js/game.js
+++ virusBoardGame/www/js/game.js
@@ -24,58 +24,68 @@ function actualizar(){
 		cx.fillRect(objetos[i].x, objetos[i].y, objetos[i].width, objetos[i].height);
 	}
 }
+//Si consideramos offset del canvas (deberia ser 0 pero si no..)
+//var x_2 = touch.pageX - canvas.offsetLeft;
+//var y_2 = touch.pageY - canvas.offsetTop
 window.onload = function(){
 	objetos = [];
 	cv = document.getElementById('canvas')
 	cx = cv.getContext('2d');
 	objetos.push({
 		x: 0, y: 0,
-		width: 100, height: 200,
+		width: 200, height: 400,
 		color: '#00f'
 	});
 	objetos.push({
 		x: 300, y: 150,
-		width: 50, height: 100,
+		width: 100, height: 200,
 		color: '#f00'
 	});
 	objetos.push({
 		x: 120, y: 150,
-		width: 50, height: 100,
+		width: 100, height: 200,
 		color: '#0f0'
 	});
 
+	//Movil - ordenador
 	cv.ontouchstart = function(event) {
+		var touch = event.touches[0];
 	//cv.onmousedown = function(event) {
+		//var touch = event;
 		console.log("Onmousedown");
 		for (var i = 0; i < objetos.length; i++) {
-			if (objetos[i].x < event.clientX
-			  && (objetos[i].width + objetos[i].x > event.clientX)
-			  && objetos[i].y < event.clientY
-			  && (objetos[i].height + objetos[i].y > event.clientY)) {
+			if (objetos[i].x < touch.pageX
+			  && (objetos[i].width + objetos[i].x > touch.pageX)
+			  && objetos[i].y < touch.pageY
+			  && (objetos[i].height + objetos[i].y > touch.pageY)) {
 				objetoActual = objetos[i];
-				inicioY = event.clientY - objetos[i].y;
-				inicioX = event.clientX - objetos[i].x;
+				console.log("Objeto "+i+" TOCADO");
+				inicioY = touch.pageY - objetos[i].y;
+				inicioX = touch.pageX - objetos[i].x;
 				break;
 			}
 		}
 	}
-
+	//Movil - ordenador
 	cv.ontouchmove = function(event) {
+		var touch = event.touches[0];
 	//cv.onmousemove = function(event) {
+		//var touch = event;
 		console.log("Onmousemove");
 		if (objetoActual != null) {
-			objetoActual.x = event.clientX - inicioX;
-			objetoActual.y = event.clientY - inicioY;
+			objetoActual.x = touch.pageX - inicioX;
+			objetoActual.y = touch.pageY - inicioY;
 		}
 		actualizar();
 	}
-
+	//Movil - ordenador
 	cv.ontouchend = function(event) {
 	//cv.onmouseup = function(event) {
 		console.log("Onmouseup");
 		objetoActual = null;
 	}
 }
+
 
 
 function takeCard(){
@@ -91,6 +101,9 @@ function takeCard(){
 
 $(document).ready(function(){
 	console.log("Document Ready");
+	console.log("Orientation before lock is: "+screen.orientation.type);
+	//Da error en el navegador, pero no para la ejecucion
+	screen.orientation.lock('landscape');
 
 	simularDatosIniciales();
 
